ee663fcfbcf32534ee5c0c2594eeac8aea5b4e9f
Shreyaa710 <shreyashar80@gmail.com>
2023-04-09 23:29:34 +0530

    yo yo

--- script.js
+++ script.js
@@ -22,3 +22,30 @@ document.querySelector(".mobicon").addEventListener("click", (el) => {
 document.querySelector(".closeicon").addEventListener("click", (el) => {
   document.querySelector(".mobileNav").classList.toggle("none");
 });
+
+// codepennnnnnnnn
+// Change option selected
+const label = document.querySelector(".dropdown__filter-selected");
+const options = Array.from(
+  document.querySelectorAll(".dropdown__select-option")
+);
+
+options.forEach((option) => {
+  option.addEventListener("click", () => {
+    label.textContent = option.textContent;
+  });
+});
+
+// Close dropdown onclick outside
+document.addEventListener("click", (e) => {
+  const toggle = document.querySelector(".dropdown__switch");
+  const element = e.target;
+
+  if (element == toggle) return;
+
+  const isDropdownChild = element.closest(".dropdown__filter");
+
+  if (!isDropdownChild) {
+    toggle.checked = false;
+  }
+});
